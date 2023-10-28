0baf1a78b0895d561c6fef43c5f1c6dad483d4af
groveripoh <67500012+groveripoh@users.noreply.github.com>
2023-10-28 18:16:37 +0800

    Update example 1.ts

--- example 1.ts
+++ example 1.ts
@@ -1,5 +1,5 @@
-
 //Ex 1 - checking exercise
+
 let a = 1 + 2
 let b = a + 3
 let c = {
@@ -10,17 +10,3 @@ let d = c.apple * 4
 console.log(a); //print a
 console.log(b); //print b
 console.log(c); //print c
-
-//----------------------------------------------------------
-//Ex 2 - more checking exercise
-function squareOf(n) {
-return n * n
-}
-squareOf(2) // evaluates to 4
-squareOf('z') // evaluates to NaN
-
-console.log(squareOf(2)); //print a
-console.log(squareOf('z')); //print b
-
-//Question : Why is there a read squiggly line under n in squareOf (n)
-//----------------------------------------------------------
